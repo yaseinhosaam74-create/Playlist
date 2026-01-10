52aba79cd246a7fa4ec7c949f5c1ed77a5dd578d
Yoursxm <yaseinhosaam74@gmail.com>
2026-01-10 23:52:56 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'music-app-v1';
+const assets = [
+  'index.html',
+  'style.css',
+  'script.js',
+  'manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
